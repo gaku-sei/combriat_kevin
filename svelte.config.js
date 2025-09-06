9739386809114fbd4410ccaa828a9ce395897127
COMBRIAT Kévin <japan.combriat.kevin@gmail.com>
2025-09-06 14:12:10 +0900

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-import env from "node:process";
-
 /** @type {import('@sveltejs/kit').Config} */
 export default {
   preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter({
-      fallback: "index.html",
+      pages: "docs",
+      assets: "docs",
+      precompress: false,
+      strict: true,
     }),
-    paths: {
-      base: env.NODE_ENV === "production" ? "/combriat-kevin" : "",
-    },
+  },
+  paths: {
+    base: process.argv.includes("dev") ? "" : "/combriat_kevin",
   },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import env from "node:process";
 
 export default defineConfig({
-  base: env.NODE_ENV === "production" ? "/combriat-kevin" : "",
   plugins: [tailwindcss(), sveltekit()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
 });
